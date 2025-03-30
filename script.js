d8e191a8eaf78550acf86f6c8cabc54f296bb033
Kira-Linux <166656040+elliotX19@users.noreply.github.com>
2025-03-30 11:18:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showProjectDetails() {
+    var details = document.getElementById("projectDetails");
+
+    // Toggle the display of the project details
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
